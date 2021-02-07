594cb207a19be61bc17e062f27fc708eb4214089
Toasterbirb <git.53jvg@aleeas.com>
2021-02-07 14:37:35 +0200

    Changed dmenu font and increased overall font size

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "mononoki:size=14";
 static char normbgcolor[]           = "#3b4252";
 static char normbordercolor[]       = "#4c566a";
 static char normfgcolor[]           = "#d8dee9";
