6b0e12157e01bc68582759bcc3e671f5f865229b
toasterbirb <anton.meuronen@protonmail.com>
2021-04-03 11:48:04 +0300

    More color options

--- config.def.h
+++ config.def.h
@@ -17,13 +17,24 @@ static const char dmenufont[]       = "mononoki:size=14";
 static char normbgcolor[]           = "#3b4252";
 static char normbordercolor[]       = "#2E3440";
 static char normfgcolor[]           = "#d8dee9";
-static char selfgcolor[]            = "#e5e9f0";
-static char selbordercolor[]        = "#bf616a";
-static char selbgcolor[]            = "#bf616a";
+static char selfgcolor[]            = "#2e3440";
+static char selbordercolor[]        = "#81a1c1";
+static char selbgcolor[]            = "#a3be8c";
+static char col_gray1[] 			= "#94A093";
+static char col_gray3[] 			= "#2e3440";
+static char col_gray4[] 			= "#000000";
+static char col_cyan[] 				= "#a3be8c";
+
 static char *colors[][3] = {
-       /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+       	/*               	fg           		bg           	border   */
+       	[SchemeNorm] 		= { normfgcolor, 	normbgcolor, 	normbordercolor },
+       	[SchemeSel]  		= { selfgcolor,  	selbgcolor,  	selbordercolor  },
+		[SchemeStatus]  	= { col_gray3, 		col_gray1,  	"#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+		[SchemeTagsSel]  	= { col_gray4, 		col_cyan,  		"#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+		[SchemeTagsNorm]  	= { col_gray3, 		col_gray1,  	"#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+		[SchemeInfoSel]  	= { col_gray4, 		col_cyan,  		"#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+		[SchemeInfoNorm]  	= { col_gray3, 		col_gray1,  	"#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+
 };
 
 /* tagging */
@@ -34,9 +45,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       										tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       										0,            0,           -1 },
+	{ "Firefox",  NULL,       NULL,       										1 << 8,       0,           -1 },
+	{ NULL,       NULL,       "Steam Guard - Computer Authorization Required",  0, 			  1, 		   -1 },
 };
 
 /* layout(s) */
@@ -51,7 +63,7 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",   		tile },    /* first entry is default */
 	{ "[M]",   		monocle },
-	//{ "[@]", 		spiral },
+	{ "[@]", 		spiral },
 	//{ "[\\]",		dwindle },
 	//{ "H[]", 		deck },
 	{ "TTT",   		bstack },
