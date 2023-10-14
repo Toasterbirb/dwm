56950aebd66653b254fee0e35ba1e5272eea08fc
Toasterbirb <git@toasterbirb.com>
2023-10-14 18:34:54 +0300

    Add vanitygaps patch

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is my personal build of dwm with some patches applied and other modificatio
 
 ## Patches
 - [keycodes](https://dwm.suckless.org/patches/keycodes/dwm-keycodes-6.4.diff)
+- [vanitygaps](https://dwm.suckless.org/patches/vanitygaps/dwm-vanitygaps-20200610-f09418b.diff)
--- config.def.h
+++ config.def.h
@@ -7,6 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -84,6 +89,7 @@ static const Key keys[] = {
 	{ MODKEY,               60,     focusmon,       {.i = +1 } },   // period
 	{ MODKEY|ShiftMask,     59,     tagmon,         {.i = -1 } },   // comma
 	{ MODKEY|ShiftMask,     60,     tagmon,         {.i = +1 } },   // period
+	{ MODKEY|Mod4Mask,      19,     togglegaps,     {0} },          // 0
 	TAGKEYS(                10,                     0)              // 1
 	TAGKEYS(                11,                     1)              // 2
 	TAGKEYS(                12,                     2)              // 3
