320a67cc3161aef0d4b40d76425165a461912608
Toasterbirb <git@toasterbirb.com>
2023-10-14 20:05:58 +0300

    Fix bugs with dmenu and master zooming

--- config.def.h
+++ config.def.h
@@ -71,7 +71,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-hp", "firefox;surf", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
@@ -85,7 +86,7 @@ static const Key keys[] = {
 	{ MODKEY|ControlMask,   55,     incnmaster,     {.i = -1 } },   // v
 	{ MODKEY,               43,     setmfact,       {.f = -0.05} }, // h
 	{ MODKEY,               46,     setmfact,       {.f = +0.05} }, // l
-	{ MODKEY,               36,     zoom,           {0} },          // Return
+	{ MODKEY|ShiftMask,     36,     zoom,           {0} },          // Return
 	{ MODKEY,               23,     view,           {0} },          // Tab
 	{ MODKEY|ShiftMask,     24,     killclient,     {0} },          // q
 	{ MODKEY,               28,     setlayout,      {.v = &layouts[0]} }, // t
--- dwm.c
+++ dwm.c
@@ -1680,6 +1680,9 @@ sigchld(int unused)
 void
 spawn(const Arg *arg)
 {
+	if (arg->v == dmenucmd)
+		dmenumon[0] = '0' + selmon->num;
+
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
