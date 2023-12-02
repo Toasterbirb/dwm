22f059fd3e6a27eb6fe758a7d00e58a88b0c5fb9
Toasterbirb <git@toasterbirb.com>
2023-12-03 01:58:46 +0200

    Add a kanagawa theme

---
+++ themes/kanagawa.h
@@ -0,0 +1,34 @@
+#pragma once
+
+/* border */
+static const char border_active[]   = "#DCD7BA";
+static const char border_inactive[] = "#1a1a24";
+
+/* tagbar */
+static const char tag_active_text[]         = "#1F1F28";
+static const char tag_active_background[]   = "#C8C093";
+static const char tag_active_indicator[]    = "#DCD7BA";
+static const char tag_active_3d_shadow[]    = "#ada67f";
+static const char tag_inactive_text[]       = "#DCD7BA";
+static const char tag_inactive_background[] = "#1F1F28";
+static const char tag_inactive_indicator[]  = "#DCD7BA";
+static const char tag_inactive_3d_shadow[]  = "#18181f";
+
+/* infobar */
+static const char info_active_text[]         = "#1F1F28";
+static const char info_active_background[]   = "#C8C093";
+static const char info_active_3d_shadow[]    = "#ada67f";
+static const char info_inactive_text[]       = "#DCD7BA";
+static const char info_inactive_background[] = "#1F1F28";
+static const char info_inactive_3d_shadow[]  = "#18181f";
+
+/* statusbar */
+static const char status_text[]       = "#DCD7BA";
+static const char status_background[] = "#1F1F28";
+static const char status_3d_shadow[]  = "#18181f";
+
+/* dmenu */
+static const char dmenu_text[]                = "#DCD7BA";
+static const char dmenu_background[]          = "#223249";
+static const char dmenu_active_text[]         = "#54546D";
+static const char dmenu_active_background[]   = "#C8C093";
