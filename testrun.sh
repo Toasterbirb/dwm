7842eec9702ea79f46179304421df595b5514c82
Toasterbirb <git@toasterbirb.com>
2023-12-03 01:15:39 +0200

    Start implementing alternatives themes

--- testrun.sh
+++ testrun.sh
@@ -9,8 +9,9 @@ Help()
    echo
    echo "Syntax: $(basename $0) [-h|d]"
    echo "options:"
-   echo "h     Print this help."
-   echo "d     Change display for test server."
+   echo "h              Print this help."
+   echo "d              Change display for test server."
+   echo "t [theme_name] Use a custom colorscheme"
    echo
    echo "example: $(basename $0) -d :2"
    echo
@@ -19,15 +20,19 @@ Help()
 # Default display
 XEPHYR_DISPLAY=":1"
 
-while getopts ':hd:' OPTION; do
+while getopts ':hdt:' OPTION; do
   case "$OPTION" in
     d)
-      XEPHYR_DISPLAY="$OPTARG"
-      echo "\$XEPHYR_DISPLAY configured to $OPTARG"
-      ;;
+		XEPHYR_DISPLAY="$OPTARG"
+		echo "\$XEPHYR_DISPLAY configured to $OPTARG"
+		;;
     h)
-      Help
-      exit;;
+		Help
+		exit;;
+	t)
+		THEME="./themes/$OPTARG"
+		echo "Setting theme to $OPTARG"
+		;;
     ?)
       Help
       exit 1;;
@@ -49,4 +54,10 @@ export DISPLAY=$XEPHYR_DISPLAY
 
 make clean
 rm -f config.h theme.h
+
+if [ -n "$THEME" ]
+then
+	ln -sr $THEME theme.h
+fi
+
 make && _relaunch
---
+++ themes/nord-red.h
@@ -0,0 +1,34 @@
+#pragma once
+
+/* border */
+static const char border_active[]   = "#cddbf9";
+static const char border_inactive[] = "#1a1a24";
+
+/* tagbar */
+static const char tag_active_text[]         = "#2c2e3e";
+static const char tag_active_background[]   = "#bf616a";
+static const char tag_active_indicator[]    = "#cddbf9";
+static const char tag_active_3d_shadow[]    = "#a6545c";
+static const char tag_inactive_text[]       = "#cddbf9";
+static const char tag_inactive_background[] = "#2c2e3e";
+static const char tag_inactive_indicator[]  = "#cddbf9";
+static const char tag_inactive_3d_shadow[]  = "#242633";
+
+/* infobar */
+static const char info_active_text[]         = "#2c2e3e";
+static const char info_active_background[]   = "#bf616a";
+static const char info_active_3d_shadow[]    = "#a6545c";
+static const char info_inactive_text[]       = "#cddbf9";
+static const char info_inactive_background[] = "#2c2e3e";
+static const char info_inactive_3d_shadow[]  = "#242633";
+
+/* statusbar */
+static const char status_text[]       = "#cddbf9";
+static const char status_background[] = "#2c2e3e";
+static const char status_3d_shadow[]  = "#242633";
+
+/* dmenu */
+static const char dmenu_text[]                = "#cddbf9";
+static const char dmenu_background[]          = "#2c2e3e";
+static const char dmenu_active_text[]         = "#2c2e3e";
+static const char dmenu_active_background[]   = "#bf616a";
